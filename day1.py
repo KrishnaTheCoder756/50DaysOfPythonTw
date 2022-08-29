b291612e971ff99edbf2777a39509bbd2c49f066
Krishna <krishnasarathishem@gmail.com>
2022-08-29 16:50:20 +0530

    Day 1 Complete

---
+++ day1.py
@@ -0,0 +1,14 @@
+# Day-1 of 50 Days Of Code By Benjamin Bennett Alexander
+
+from math import sqrt
+
+
+def divide_or_square(n):
+    if n % 5 == 0:
+        square_root = n ** 0.5
+        return f"We found your number is divisible by 5, so the square root of your number is {square_root}"
+    else:
+        return f"As your number is not divisible by 5, the remainder of your number divided by 5 is {n%5}"
+
+n1 = input("Enter your number: ")
+divide_or_square(n1)
